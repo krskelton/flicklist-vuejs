7181fe1556e1ae7b3a21a4596b45db6eb4b38b39
Kristin Skelton <kskelton82@gmail.com>
2019-09-12 20:42:16 -0600

    Final Studio 2

--- js/flicklist.js
+++ js/flicklist.js
@@ -1,7 +1,7 @@
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "f6bd21e39046f6a08df40d82ad8cffd3"
+  token: "16daec530c53f1a885429ca446fa8ebc"
 }
 
 var flicklistView = new Vue({
@@ -12,7 +12,8 @@ var flicklistView = new Vue({
 			// Whenever it changes, Vue will automatically re-render
 			// the html for us.
 			watchlistItems: [],
-      browseItems: [],
+			browseItems: [],
+			searchTerm: null
       // TODO 8B
 		};
 	},
@@ -26,24 +27,32 @@ var flicklistView = new Vue({
 			fetch(`${api.root}/discover/movie?api_key=${api.token}`)
 					.then(resp => resp.ok ? resp.json() : Promise.reject(resp))
 					.then((response) => {
-						console.log("We got a response from The Movie DB!");
-						console.log(response);
+						// console.log("We got a response from The Movie DB!");
+						// console.log(response);
 
 						this.browseItems = response.results;
 
 					});
     },
     searchMovies: function(searchTerm) {
-      // Make an AJAX request to the /search/movie endpoint
-      // of the API, using the query string that was passed in.
-      //
-      // if successful, update this.browseItems appropriately.
+      /*Make an AJAX request to the /search/movie endpoint of the API, using the query string that was passed in.
+      // if successful, update this.browseItems appropriately.*/
       // This update will automatically trigger a re-render.
-      console.log(`searching for movies with "${searchTerm}" in their title...`);
+	  console.log(`searching for movies with "${searchTerm}" in their title...`);
 
       // TODO 9
       // implement this function as described in the comment above
-      // you can use the body of discoverMovies as a jumping off point
+	  // you can use the body of discoverMovies as a jumping off point
+	  fetch(`${api.root}/search/movie?api_key=${api.token}&query=${searchTerm}`)	  	
+		.then(resp => resp.ok ? resp.json() : Promise.reject(resp))
+		.then((response) => {
+			console.log(searchTerm);
+			console.log("We got a response from The Movie DB!");
+			console.log(response);
+
+			this.browseItems = response.results;
+
+		});
     },
 		addToWatchlist: function(movie) {
 			this.watchlistItems.push(movie);
@@ -51,5 +60,5 @@ var flicklistView = new Vue({
 	},
 	mounted: function () {
 		this.discoverMovies();
-	},
+	}
 });
